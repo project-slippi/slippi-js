e91edf4565b0484c87db91d1fe94b7b259e52571
Max Timkovich <mtimkovich@gmail.com>
2019-06-01 11:23:19 -0700

    add Default as index 0

--- src/melee/characters.js
+++ src/melee/characters.js
@@ -2,132 +2,132 @@ const externalCharacters = [{
   id: 0,
   name: "Captain Falcon",
   shortName: "Falcon",
-  colors: ["Black", "Red", "White", "Green", "Blue"],
+  colors: ["Default", "Black", "Red", "White", "Green", "Blue"],
 }, {
   id: 1,
   name: "Donkey Kong",
   shortName: "DK",
-  colors: ["Black", "Red", "Blue", "Green"],
+  colors: ["Default", "Black", "Red", "Blue", "Green"],
 }, {
   id: 2,
   name: "Fox",
   shortName: "Fox",
-  colors: ["Red", "Blue", "Green"],
+  colors: ["Default", "Red", "Blue", "Green"],
 }, {
   id: 3,
   name: "Mr. Game & Watch",
   shortName: "G&W",
-  colors: ["Red", "Blue", "Green"],
+  colors: ["Default", "Red", "Blue", "Green"],
 }, {
   id: 4,
   name: "Kirby",
   shortName: "Kirby",
-  colors: ["Yellow", "Blue", "Red", "Green", "White"],
+  colors: ["Default", "Yellow", "Blue", "Red", "Green", "White"],
 }, {
   id: 5,
   name: "Bowser",
   shortName: "Bowser",
-  colors: ["Red", "Blue", "Black"],
+  colors: ["Default", "Red", "Blue", "Black"],
 }, {
   id: 6,
   name: "Link",
   shortName: "Link",
-  colors: ["Red", "Blue", "Black", "White"],
+  colors: ["Default", "Red", "Blue", "Black", "White"],
 }, {
   id: 7,
   name: "Luigi",
   shortName: "Luigi",
-  colors: ["White", "Blue", "Red"],
+  colors: ["Default", "White", "Blue", "Red"],
 }, {
   id: 8,
   name: "Mario",
   shortName: "Mario",
-  colors: ["Yellow", "Black", "Blue", "Green"],
+  colors: ["Default", "Yellow", "Black", "Blue", "Green"],
 }, {
   id: 9,
   name: "Marth",
   shortName: "Marth",
-  colors: ["Red", "Green", "Black", "White"],
+  colors: ["Default", "Red", "Green", "Black", "White"],
 }, {
   id: 10,
   name: "Mewtwo",
   shortName: "Mewtwo",
-  colors: ["Red", "Blue", "Green"],
+  colors: ["Default", "Red", "Blue", "Green"],
 }, {
   id: 11,
   name: "Ness",
   shortName: "Ness",
-  colors: ["Yellow", "Blue", "Green"],
+  colors: ["Default", "Yellow", "Blue", "Green"],
 }, {
   id: 12,
   name: "Peach",
   shortName: "Peach",
-  colors: ["Daisy", "White", "Blue", "Green"],
+  colors: ["Default", "Daisy", "White", "Blue", "Green"],
 }, {
   id: 13,
   name: "Pikachu",
   shortName: "Pikachu",
-  colors: ["Red", "Party Hat", "Cowboy Hat"],
+  colors: ["Default", "Red", "Party Hat", "Cowboy Hat"],
 }, {
   id: 14,
   name: "Ice Climbers",
   shortName: "ICs",
-  colors: ["Green", "Orange", "Red"],
+  colors: ["Default", "Green", "Orange", "Red"],
 }, {
   id: 15,
   name: "Jigglypuff",
   shortName: "Puff",
-  colors: ["Red", "Blue", "Headband", "Crown"],
+  colors: ["Default", "Red", "Blue", "Headband", "Crown"],
 }, {
   id: 16,
   name: "Samus",
   shortName: "Samus",
-  colors: ["Pink", "Black", "Green", "Purple"],
+  colors: ["Default", "Pink", "Black", "Green", "Purple"],
 }, {
   id: 17,
   name: "Yoshi",
   shortName: "Yoshi",
-  colors: ["Red", "Blue", "Yellow", "Pink", "Cyan"],
+  colors: ["Default", "Red", "Blue", "Yellow", "Pink", "Cyan"],
 }, {
   id: 18,
   name: "Zelda",
   shortName: "Zelda",
-  colors: ["Red", "Blue", "Green", "White"],
+  colors: ["Default", "Red", "Blue", "Green", "White"],
 }, {
   id: 19,
   name: "Sheik",
   shortName: "Sheik",
-  colors: ["Red", "Blue", "Green", "White"],
+  colors: ["Default", "Red", "Blue", "Green", "White"],
 }, {
   id: 20,
   name: "Falco",
   shortName: "Falco",
-  colors: ["Red", "Blue", "Green"],
+  colors: ["Default", "Red", "Blue", "Green"],
 }, {
   id: 21,
   name: "Young Link",
   shortName: "YLink",
-  colors: ["Red", "Blue", "White", "Black"],
+  colors: ["Default", "Red", "Blue", "White", "Black"],
 }, {
   id: 22,
   name: "Dr. Mario",
   shortName: "Doc",
-  colors: ["Red", "Blue", "Green", "Black"],
+  colors: ["Default", "Red", "Blue", "Green", "Black"],
 }, {
   id: 23,
   name: "Roy",
   shortName: "Roy",
-  colors: ["Red", "Blue", "Green", "Yellow"],
+  colors: ["Default", "Red", "Blue", "Green", "Yellow"],
 }, {
   id: 24,
   name: "Pichu",
   shortName: "Pichu",
-  colors: ["Red", "Blue", "Green"],
+  colors: ["Default", "Red", "Blue", "Green"],
 }, {
   id: 25,
   name: "Ganondorf",
   shortName: "Ganon",
-  colors: ["Red", "Blue", "Green", "Purple"],
+  colors: ["Default", "Red", "Blue", "Green", "Purple"],
 }];
 
 export function getAllCharacters() {
@@ -146,13 +146,6 @@ export function getCharacterShortName(externalCharacterId) {
 // Return a human-readable color from a characterCode.
 export function getCharacterColorName(externalCharacterId, characterColor) {
   const character = getCharacterInfo(externalCharacterId) || {};
-  if (!character || characterColor < 0 || characterColor > len(character)) {
-    return null;
-  }
-
-  if (characterColor === 0) {
-    return "Neutral";
-  }
-
-  return character.colors[characterColor-1];
+  const colors = character.colors || [];
+  return colors[characterColor] || null;
 }
