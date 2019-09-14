ef38b6c5f3c0f0d85e381f2fe627b7f7d92e3f89
Vince Au <vinceau09@gmail.com>
2019-09-14 16:16:13 +1000

    Fix metadata calculation

--- src/utils/slpStream.ts
+++ src/utils/slpStream.ts
@@ -15,27 +15,76 @@ export class SlpStream {
 
   constructor(stream: Readable) {
     stream.on('readable', () => {
-      this.setMetadata(stream);
       let chunk;
       while (null !== (chunk = stream.read())) {
+        this.setMetadata(chunk);
         console.log('chunk: ', chunk);
       }
     })
   }
 
-  setMetadata(stream: Readable): void {
+  setMetadata(chunk: Buffer): void {
     if (this.metadataSet) {
       return;
     }
-    this.rawDataPosition = getRawDataPosition(stream);
-    console.log(this.rawDataPosition);
-    this.rawDataLength = getRawDataLength(stream);
-    console.log(this.rawDataLength);
-    // this.metadataPosition = this.rawDataPosition + this.rawDataLength + 10; // remove metadata string
-    // this.metadataLength = getMetadataLength(chunk, this.metadataPosition);
-    // this.messageSizes = getMessageSizes(chunk, this.rawDataPosition);
+    const rawDataPosition = getRawDataPosition(chunk);
+    const rawDataLength = getRawDataLength(chunk, rawDataPosition);
+    const metadataPosition = rawDataPosition + rawDataLength + 10; // remove metadata string
+    const metadataLength = getMetadataLength(chunk, metadataPosition);
+    const messageSizes = getMessageSizes(chunk, rawDataPosition);
+    console.log(rawDataPosition);
+    console.log(rawDataLength);
+    console.log(metadataPosition);
+    console.log(metadataLength);
+    console.log(messageSizes);
     this.metadataSet = true;
   }
+}
+
+// This function gets the position where the raw data starts
+function getRawDataPosition(chunk: Buffer): number {
+  const buffer = new Uint8Array(1);
+  readRef(chunk, buffer, 0, buffer.length, 0);
+
+  if (buffer[0] === 0x36) {
+    return 0;
+  }
+
+  if (buffer[0] !== '{'.charCodeAt(0)) {
+    return 0; // return error?
+  }
+
+  return 15;
+}
+
+function readRef(chunk: Buffer, buffer: Uint8Array, offset: number, length: number, position: number): number {
+  return chunk.copy(buffer, offset, position, position + length);
+}
+
+function getRawDataLength(chunk: Buffer, position: number): number {
+  const fileSize = chunk.length;
+  if (position === 0) {
+    return fileSize;
+  }
+
+  const buffer = new Uint8Array(4);
+  readRef(chunk, buffer, 0, buffer.length, position - 4);
+
+  const rawDataLen = buffer[0] << 24 | buffer[1] << 16 | buffer[2] << 8 | buffer[3];
+  if (rawDataLen > 0) {
+    // If this method manages to read a number, it's probably trustworthy
+    return rawDataLen;
+  }
+
+  // If the above does not return a valid data length,
+  // return a file size based on file length. This enables
+  // some support for severed files
+  return fileSize - position;
+}
+
+function getMetadataLength(chunk: Buffer, position: number): number {
+  const len = chunk.length;
+  return len - position - 1;
 }
 
 function getMessageSizes(chunk: Buffer, position: number): {
@@ -54,7 +103,7 @@ function getMessageSizes(chunk: Buffer, position: number): {
   }
 
   const buffer = new Uint8Array(2);
-  readStreamRef(chunk, buffer, 0, buffer.length, position);
+  readRef(chunk, buffer, 0, buffer.length, position);
   if (buffer[0] !== Command.MESSAGE_SIZES) {
     return {};
   }
@@ -63,7 +112,7 @@ function getMessageSizes(chunk: Buffer, position: number): {
   messageSizes[0x35] = payloadLength;
 
   const messageSizesBuffer = new Uint8Array(payloadLength - 1);
-  readStreamRef(chunk, messageSizesBuffer, 0, messageSizesBuffer.length, position + 2);
+  readRef(chunk, messageSizesBuffer, 0, messageSizesBuffer.length, position + 2);
   for (let i = 0; i < payloadLength - 1; i += 3) {
     const command = messageSizesBuffer[i];
 
@@ -72,39 +121,4 @@ function getMessageSizes(chunk: Buffer, position: number): {
   }
 
   return messageSizes;
-}
-
-
-function readStreamRef(sourceBuffer: Buffer, buffer: Uint8Array, offset: number, length: number, position: number): number {
-    return sourceBuffer.copy(buffer, offset, position, position + length);
-}
-
-function getRawDataPosition(stream: Readable): number {
-  const buffer = stream.read(1);
-  if (buffer[0] === 0x36) {
-    return 0;
-  }
-  if (buffer[0] !== '{'.charCodeAt(0)) {
-    return 0; // return error?
-  }
-  return 15;
-}
-
-function getRawDataLength(stream: Readable): number {
-  const buffer = stream.read(4);
-  const rawDataLen = buffer[0] << 24 | buffer[1] << 16 | buffer[2] << 8 | buffer[3];
-  if (rawDataLen > 0) {
-    // If this method manages to read a number, it's probably trustworthy
-    return rawDataLen;
-  }
-
-  // If the above does not return a valid data length,
-  // return a file size based on file length. This enables
-  // some support for severed files
-  return stream.readableLength
-}
-
-function getMetadataLength(chunk: Buffer, position: number): number {
-  const len = chunk.length;
-  return len - position - 1;
 }
