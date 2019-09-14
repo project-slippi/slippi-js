c87d16924defbb25d846af33eb3d24ea4e107d44
Vince Au <vinceau09@gmail.com>
2019-09-14 22:32:51 +1000

    Add code to compare original

--- src/index.ts
+++ src/index.ts
@@ -19,26 +19,30 @@ export {
 };
 
 
-const stream = fs.createReadStream("slp/sheik_vs_ics_yoshis.slp");
-const parser = new SlpParser();
-const slp = new SlpStream(stream);
-slp.on(SlpEvent.GAME_START, (command: Command, payload: GameStartType) => {
-  parser.handleGameStart(payload);
-});
+// const stream = fs.createReadStream("slp/sheik_vs_ics_yoshis.slp");
+// const parser = new SlpParser();
+// const slp = new SlpStream(stream);
+// slp.on(SlpEvent.GAME_START, (command: Command, payload: GameStartType) => {
+//   parser.handleGameStart(payload);
+// });
 
-slp.on(SlpEvent.POST_FRAME_UPDATE, (command: Command, payload: PostFrameUpdateType) => {
-  parser.handlePostFrameUpdate(payload);
-});
+// slp.on(SlpEvent.POST_FRAME_UPDATE, (command: Command, payload: PostFrameUpdateType) => {
+//   parser.handlePostFrameUpdate(payload);
+// });
 
-slp.on(SlpEvent.PRE_FRAME_UPDATE, (command: Command, payload: PreFrameUpdateType) => {
-  parser.handleFrameUpdate(command, payload);
-});
+// slp.on(SlpEvent.PRE_FRAME_UPDATE, (command: Command, payload: PreFrameUpdateType) => {
+//   parser.handleFrameUpdate(command, payload);
+// });
 
-slp.on(SlpEvent.GAME_END, (command: Command, payload: GameEndType) => {
-  parser.handleGameEnd(payload);
-  console.log(parser.getSettings());
-  const frames = parser.getFrames();
-  console.log(JSON.stringify(frames[1]));
-});
+// slp.on(SlpEvent.GAME_END, (command: Command, payload: GameEndType) => {
+//   parser.handleGameEnd(payload);
+//   console.log(parser.getSettings());
+//   const frames = parser.getFrames();
+//   console.log(JSON.stringify(frames[1]));
+// });
+
+const game = new SlippiGame("slp/sheik_vs_ics_yoshis.slp");
+const frames = game.getFrames();
+console.log(JSON.stringify(frames[1]));
 
 export default SlippiGame;
