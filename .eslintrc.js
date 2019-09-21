0c1dd1c1a5bf63f2ccc5a686613028774206e959
jlaferri <jas.laferriere@gmail.com>
2019-09-21 13:18:00 +0200

    add lint rule for switch case indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports =  {
     sourceType:  'module',  // Allows for the use of imports
   },
   rules: {
+    "@typescript-eslint/indent": ["error", 2, {"SwitchCase": 0}],
     "@typescript-eslint/no-use-before-define": "off",
     "@typescript-eslint/explicit-member-accessibility": "warn"
   }
