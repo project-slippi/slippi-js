005b18a39b9565481d55a2fa8da7d381547e6e3a
Vince Au <vinceau09@gmail.com>
2020-07-10 12:43:47 +1000

    Add test for finalizedFrame

---
+++ test/latest-frame.spec.ts
@@ -0,0 +1,39 @@
+import SlippiGame, {
+  pipeFileContents,
+  Command,
+  SlpStreamMode,
+  SlpStream,
+  Frames,
+  SlpStreamEvent,
+  FrameBookendType,
+} from "../src";
+
+const MAX_ROLLBACK_FRAMES = 7;
+
+describe("when reading last finalised frame", () => {
+  it("should never decrease", async () => {
+    const testFile = "slp/finalizedFrame.slp";
+    const stream = new SlpStream({
+      mode: SlpStreamMode.MANUAL,
+    });
+
+    let lastFinalizedFrame = Frames.FIRST - 1;
+
+    stream.on("slp-command", (data: SlpStreamEvent) => {
+      switch (data.command) {
+        case Command.FRAME_BOOKEND:
+          const payload = data.payload as FrameBookendType;
+          expect(payload.latestFinalizedFrame).toBeGreaterThanOrEqual(lastFinalizedFrame);
+          // We also expect it to be within 7 frames of the current frame
+          expect(payload.latestFinalizedFrame).toBeGreaterThanOrEqual(payload.frame - MAX_ROLLBACK_FRAMES);
+          lastFinalizedFrame = payload.latestFinalizedFrame;
+      }
+    });
+    await pipeFileContents(testFile, stream);
+
+    // The last finalized frame should be the same as what's recorded in the metadata
+    const game = new SlippiGame(testFile);
+    const metadata = game.getMetadata();
+    expect(metadata.lastFrame).toEqual(lastFinalizedFrame);
+  });
+});
