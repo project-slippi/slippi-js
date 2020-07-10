5c3e8f54ea5bd5259568bc70fc0c359542cc2ffb
Vince Au <vinceau09@gmail.com>
2020-07-10 21:59:25 +1000

    Fix quotations in scripts

--- scripts/realtimeFileReads.js
+++ scripts/realtimeFileReads.js
@@ -1,12 +1,12 @@
-const { default: SlippiGame } = require("@slippi/sdk");
-const chokidar = require("chokidar");
-const _ = require("lodash");
+const { default: SlippiGame } = require('@slippi/sdk');
+const chokidar = require('chokidar');
+const _ = require('lodash');
 
 const listenPath = process.argv[2];
 console.log(`Listening at: ${listenPath}`);
 
 const watcher = chokidar.watch(listenPath, {
-  ignored: "!*.slp", // TODO: This doesn't work. Use regex?
+  ignored: '!*.slp', // TODO: This doesn't work. Use regex?
   depth: 0,
   persistent: true,
   usePolling: true,
@@ -14,12 +14,12 @@ const watcher = chokidar.watch(listenPath, {
 });
 
 const gameByPath = {};
-watcher.on("change", (path) => {
+watcher.on('change', (path) => {
   const start = Date.now();
 
   let gameState, settings, stats, frames, latestFrame, gameEnd;
   try {
-    let game = _.get(gameByPath, [path, "game"]);
+    let game = _.get(gameByPath, [path, 'game']);
     if (!game) {
       console.log(`New file at: ${path}`);
       game = new SlippiGame(path);
@@ -32,7 +32,7 @@ watcher.on("change", (path) => {
       };
     }
 
-    gameState = _.get(gameByPath, [path, "state"]);
+    gameState = _.get(gameByPath, [path, 'state']);
 
     settings = game.getSettings();
 
@@ -57,7 +57,7 @@ watcher.on("change", (path) => {
 
   console.log(`We have ${_.size(frames)} frames.`);
   _.forEach(settings.players, (player) => {
-    const frameData = _.get(latestFrame, ["players", player.playerIndex]);
+    const frameData = _.get(latestFrame, ['players', player.playerIndex]);
     if (!frameData) {
       return;
     }
@@ -96,14 +96,14 @@ watcher.on("change", (path) => {
     // NOTE: These values and the quitter index will not work until 2.0.0 recording code is
     // NOTE: used. This code has not been publicly released yet as it still has issues
     const endTypes = {
-      1: "TIME!",
-      2: "GAME!",
-      7: "No Contest",
+      1: 'TIME!',
+      2: 'GAME!',
+      7: 'No Contest',
     };
 
-    const endMessage = _.get(endTypes, gameEnd.gameEndMethod) || "Unknown";
+    const endMessage = _.get(endTypes, gameEnd.gameEndMethod) || 'Unknown';
 
-    const lrasText = gameEnd.gameEndMethod === 7 ? ` | Quitter Index: ${gameEnd.lrasInitiatorIndex}` : "";
+    const lrasText = gameEnd.gameEndMethod === 7 ? ` | Quitter Index: ${gameEnd.lrasInitiatorIndex}` : '';
     console.log(`[Game Complete] Type: ${endMessage}${lrasText}`);
   }
 
