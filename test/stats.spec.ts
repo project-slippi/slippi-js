4f5b8c177b0ed0ce1c24d72fc5f516c3c740c23d
Vince Au <vinceau09@gmail.com>
2021-01-28 00:18:39 +1100

    Add L cancel stats calculation test

---
+++ test/stats.spec.ts
@@ -0,0 +1,16 @@
+import { SlippiGame } from "../src";
+
+describe("when calculating stats", () => {
+  it("should correctly calculate L cancel counts", () => {
+    const game = new SlippiGame("slp/lCancel.slp");
+    const stats = game.getStats();
+    const p1Success = stats.actionCounts[0].lCancelSuccessCount;
+    const p1Fail = stats.actionCounts[0].lCancelFailCount;
+    expect(p1Success).toBe(3);
+    expect(p1Fail).toBe(4);
+    const p2Success = stats.actionCounts[1].lCancelSuccessCount;
+    const p2Fail = stats.actionCounts[1].lCancelFailCount;
+    expect(p2Success).toBe(5);
+    expect(p2Fail).toBe(4);
+  });
+});
