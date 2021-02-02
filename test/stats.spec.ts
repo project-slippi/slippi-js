20dcad26af304dfafd4bef699115ce429991cada
Home Desktop <k9theassassin@gmail.com>
2021-02-01 20:25:04 -0500

    Added static stats comparison

--- test/stats.spec.ts
+++ test/stats.spec.ts
@@ -5,12 +5,72 @@ const expectedThrow = {
   up: 1,
   forward: 1,
   back: 2,
-  down: 1
+  down: 1,
 };
 
 const expectedGrab = {
   success: 6,
-  fail: 2
+  fail: 2,
+};
+const slippiDefault = {
+  playerIndex: 0,
+  opponentIndex: [2],
+  inputCounts: {
+    buttons: 150,
+    triggers: 0,
+    cstick: 35,
+    joystick: 309,
+    total: 494,
+  },
+  conversionCount: 8,
+  totalDamage: 359.28001403808594,
+  killCount: 4,
+  successfulConversions: {
+    count: 6,
+    total: 8,
+    ratio: 0.75,
+  },
+  inputsPerMinute: {
+    count: 494,
+    total: 1.0369444444444444,
+    ratio: 476.39967854272703,
+  },
+  digitalInputsPerMinute: {
+    count: 150,
+    total: 1.0369444444444444,
+    ratio: 144.65577283686042,
+  },
+  openingsPerKill: {
+    count: 8,
+    total: 4,
+    ratio: 2,
+  },
+  damagePerOpening: {
+    count: 359.28001403808594,
+    total: 8,
+    ratio: 44.91000175476074,
+  },
+  neutralWinRatio: [
+    {
+      count: 8,
+      total: 8,
+      ratio: 1,
+    },
+  ],
+  counterHitRatio: [
+    {
+      count: 0,
+      total: 2,
+      ratio: 0,
+    },
+  ],
+  beneficialTradeRatio: [
+    {
+      count: 0,
+      total: 0,
+      ratio: null,
+    },
+  ],
 };
 
 describe("when calculating stats", () => {
@@ -38,9 +98,14 @@ describe("when calculating stats", () => {
     const p2Grabs = stats.actionCounts[1].grabCounts;
     expect(p2Grabs).toEqual(expectedGrab);
   });
+  it("should match output of test.slp", () => {
+    const game = new SlippiGame("slp/test.slp");
+    const stats = game.getStats();
+    expect(stats.overall[0]).toEqual(slippiDefault);
+  });
 });
 describe("when using common functions", () => {
   it("Should return false if required", () => {
-    expect(didLoseStock(undefined,undefined)).toEqual(false)
+    expect(didLoseStock(undefined, undefined)).toEqual(false);
   });
 });
