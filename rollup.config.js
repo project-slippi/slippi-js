db29cba65649769b2e5f7a0360c820dd90f1e6f3
Vince Au <vinceau09@gmail.com>
2022-01-10 23:08:24 +1100

    asdf

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,25 @@
 import resolve from "@rollup/plugin-node-resolve";
-
 import autoExternal from "rollup-plugin-auto-external";
 import typescript from "rollup-plugin-typescript2";
-import { terser } from "rollup-plugin-terser";
-
-import pkg from "./package.json";
-
-const minifyExtension = (pathToFile) => pathToFile.replace(/\.js$/, ".min.js");
 
 export default {
   input: "src/index.ts",
   output: [
     {
-      file: pkg.main,
+      dir: "dist/cjs",
       format: "cjs",
     },
     {
-      file: minifyExtension(pkg.main),
-      format: "cjs",
-    },
-    {
-      file: pkg.module,
-      format: "es",
+      dir: "dist/esm",
+      format: "esm",
+      preserveModules: true,
     },
   ],
   plugins: [
-    terser({
-      include: [/^.+\.min\.js$/],
-    }),
     autoExternal(),
     resolve(),
     typescript({
-      typescript: require("typescript"),
+      useTsconfigDeclarationDir: true,
     }),
   ],
 };
