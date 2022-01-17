63ef6a31f50e7a28fd09f74a18c9e9ba94fedd58
Vince Au <vinceau09@gmail.com>
2022-01-17 19:00:58 +1100

    fix missing lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
   parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  extends: [
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "prettier",
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
   parserOptions: {
     project: "tsconfig.json",
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
