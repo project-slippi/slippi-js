a76b566538c0a44d73b57581d109ca3c6f0ccfdb
Vince Au <vince@canva.com>
2022-10-17 00:58:29 +1100

    simplify function

--- src/utils/slpReader.ts
+++ src/utils/slpReader.ts
@@ -454,19 +454,15 @@ export function parseMessage(command: Command, payload: Uint8Array): EventPayloa
         latestFinalizedFrame: readInt32(view, 0x5),
       };
     case Command.GAME_END:
-      const getPlacementObject = (playerIndex: number): PlacementType => {
-        const data = readInt8(view, 0x3 + playerIndex);
-
-        return {
-          playerIndex: playerIndex,
-          position: data,
-        };
-      };
+      const placements = [0, 1, 2, 3].map((playerIndex): PlacementType => {
+        const position = readInt8(view, 0x3 + playerIndex);
+        return { playerIndex, position };
+      });
 
       return {
         gameEndMethod: readUint8(view, 0x1),
         lrasInitiatorIndex: readInt8(view, 0x2),
-        placements: [0, 1, 2, 3].map(getPlacementObject),
+        placements,
       };
     case Command.GECKO_LIST:
       const codes: GeckoCodeType[] = [];
