e1548396ec0ce7f6866ee3790ac16be13c688b41
Calvin Keats <calvin.keats@gmail.com>
2022-12-18 13:50:30 -0600

    include .slp for homeRun test

--- test/homeRun.spec.ts
+++ test/homeRun.spec.ts
@@ -1,3 +1,4 @@
+import { SlippiGame } from "../src";
 import { positionToHomeRunDistance } from "../src/utils/homeRunDistance";
 
 describe("when processing homerun contest replays", () => {
@@ -8,7 +9,9 @@ describe("when processing homerun contest replays", () => {
   });
 
   it("should correctly calculate the distance for positive-distance hits", () => {
-    const inGameUnits = 3331.287353515625;
+    const game = new SlippiGame("slp/homeRun_positive.slp");
+    // get sandbag's final X position
+    const inGameUnits = game.getLatestFrame()?.players[1]?.post.positionX as number;
     const homeRunDistance = positionToHomeRunDistance(inGameUnits);
     expect(homeRunDistance).toBeCloseTo(1070.9, 0);
   });
