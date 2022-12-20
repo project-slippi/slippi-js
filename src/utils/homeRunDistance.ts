053c22876e93fb4316a16304481265c8bd199832
Calvin Keats <calvin.keats@gmail.com>
2022-12-19 19:22:41 -0600

    move constants

--- src/utils/homeRunDistance.ts
+++ src/utils/homeRunDistance.ts
@@ -1,16 +1,16 @@
+const FEET_CONVERSION_FACTOR = 0.952462;
+const METERS_CONVERSION_FACTOR = 1.04167;
+
 export function positionToHomeRunDistance(distance: number, units: "feet" | "meters" = "feet"): number {
-  // In NTSC vs. PAL, meters are different sizes
-  const feetModeConversionFactor = 0.952462;
-  const metersModeConversionFactor = 1.04167;
   let score = 0;
 
   if (units === "feet") {
-    score = 10 * Math.floor(distance - 70 * feetModeConversionFactor);
+    score = 10 * Math.floor(distance - 70 * FEET_CONVERSION_FACTOR);
     // convert to float32
     score = Math.fround(score);
     score = Math.floor((score / 30.4788) * 10) / 10;
   } else {
-    score = 10 * Math.floor(distance - 70 * metersModeConversionFactor);
+    score = 10 * Math.floor(distance - 70 * METERS_CONVERSION_FACTOR);
     // convert to float32
     score = Math.fround(score);
     score = Math.floor((score / 100) * 10) / 10;
