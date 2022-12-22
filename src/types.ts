b286abc7206b0765de1ac615c33a7147470b7f3f
Nikhil Narayana <nikhil.narayana@live.com>
2022-12-21 17:59:09 -0800

    refactor: Index -> Number (#118)
    * Index -> Num
    
    * Num -> Number

--- src/types.ts
+++ src/types.ts
@@ -75,8 +75,8 @@ export interface GameStartType {
 
 interface MatchInfo {
   matchId: string | null;
-  gameIndex: number | null;
-  tiebreakerIndex: number | null;
+  gameNumber: number | null;
+  tiebreakerNumber: number | null;
 }
 
 export interface FrameStartType {
