78c9a4eb21c64fecb5585170c480942a7e82425d
Vince Au <vinceau09@gmail.com>
2025-11-14 01:00:02 +1100

    i think this actually works

--- dts.config.js
+++ dts.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   rollup(config, opts) {
+    // Get the source file from environment variable or default
+    const source = process.env.DTS_SOURCE || "./src/index.ts";
+
     if (opts.format === "esm") {
       config.output = {
         ...config.output,
@@ -9,6 +12,10 @@ module.exports = {
       };
       delete config.output.file;
     }
+
+    // Set the input source
+    config.input = source;
+
     return config;
   },
 };
