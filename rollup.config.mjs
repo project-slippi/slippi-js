cd426dc8d7b9587c1cb13fdc5c5f13df6ed969df
Vince Au <vinceau09@gmail.com>
2026-06-04 21:47:45 +0900

    chore: Re-enable source maps (#178)
    * add source maps
    
    * also add ts source maps
    
    * update rollup dts plugin
    
    * integrate source map into dts plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -48,7 +48,7 @@ const createBrowserOutput = (dir, format) => ({
   format,
   entryFileNames: `[name].${format === "esm" ? "esm.js" : "cjs"}`,
   chunkFileNames: `[name].${format === "esm" ? "esm.js" : "cjs"}`,
-  sourcemap: "hidden",
+  sourcemap: true,
   exports: "named",
   // This fixes the lodash ESM import issue as described here:
   // https://github.com/project-slippi/slippi-js/issues/168
@@ -72,7 +72,7 @@ const createNodeOutput = (dir, format) => ({
   preserveModules: true,
   preserveModulesRoot: "src",
   entryFileNames: `[name].${format === "esm" ? "esm.js" : "cjs"}`,
-  sourcemap: "hidden",
+  sourcemap: true,
   exports: "named",
   // This fixes the lodash ESM import issue as described here:
   // https://github.com/project-slippi/slippi-js/issues/168
@@ -167,7 +167,7 @@ export default defineConfig([
       index: "src/browser/index.ts",
       "index.node": "src/node/index.ts",
     },
-    output: { dir: "dist", format: "esm" },
+    output: { dir: "dist", format: "esm", sourcemap: true },
     external,
     plugins: [
       dts({
@@ -178,6 +178,7 @@ export default defineConfig([
           // Skip lib checks to speed up type resolution
           skipLibCheck: true,
         },
+        sourcemap: true,
       }),
     ],
   },
